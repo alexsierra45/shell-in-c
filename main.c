3c6f33a43ae9e8298f741a94617bc72eda089a01
Alex <alexsierraalcala@gmail.com>
2023-03-24 13:08:25 -0400

    first use of shell

--- main.c
+++ main.c
@@ -26,22 +26,6 @@ int lsh_num_builtins() {
   return sizeof(builtin_str) / sizeof(char *);
 }
 
-void lsh_loop(void) {
-  char *line;
-  char **args;
-  int status;
-
-  do {
-    printf("> ");
-    line = lsh_read_line();
-    args = lsh_split_line(line);
-    status = lsh_execute(args);
-
-    free(line);
-    free(args);
-  } while (status);
-}
-
 char *lsh_read_line(void) {
   char *line = NULL;
   ssize_t bufsize = 0; // have getline allocate a buffer for us
@@ -90,23 +74,6 @@ char **lsh_split_line(char *line) {
   return tokens;
 }
 
-int lsh_execute(char **args) {
-  int i;
-
-  if (args[0] == NULL) {
-    // An empty command was entered.
-    return 1;
-  }
-
-  for (i = 0; i < lsh_num_builtins(); i++) {
-    if (strcmp(args[0], builtin_str[i]) == 0) {
-      return (*builtin_func[i])(args);
-    }
-  }
-
-  return lsh_launch(args);
-}
-
 int lsh_launch(char **args) {
   pid_t pid, wpid;
   int status;
@@ -129,6 +96,23 @@ int lsh_launch(char **args) {
   }
 
   return 1;
+}
+
+int lsh_execute(char **args) {
+  int i;
+
+  if (args[0] == NULL) {
+    // An empty command was entered.
+    return 1;
+  }
+
+  for (i = 0; i < lsh_num_builtins(); i++) {
+    if (strcmp(args[0], builtin_str[i]) == 0) {
+      return (*builtin_func[i])(args);
+    }
+  }
+
+  return lsh_launch(args);
 }
 
 // Builtin function implementations.
@@ -159,6 +143,22 @@ int lsh_help(char **args) {
 
 int lsh_exit(char **args) {
   return 0;
+}
+
+void lsh_loop(void) {
+  char *line;
+  char **args;
+  int status;
+
+  do {
+    printf("> ");
+    line = lsh_read_line();
+    args = lsh_split_line(line);
+    status = lsh_execute(args);
+
+    free(line);
+    free(args);
+  } while (status);
 }
 
 int main(int arcg, char *argv[]) {
