18392fb3827f831e4fa91f8c90d2dcdbb9e2892b
Alex <alexsierraalcala@gmail.com>
2023-04-11 03:30:20 -0400

    almost sleeping

---
+++ builtin.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+
+// Function Declarations for builtin shell commands:
+int shell_cd(char **args);
+int shell_help(char **args);
+int shell_exit(char **args);
+
+// List of builtin commands, followed by their corresponding functions.
+char *builtin_str[] = {
+  "cd",
+  "help",
+  "exit"
+};
+
+int (*builtin_func[]) (char **) = {
+  &shell_cd,
+  &shell_help,
+  &shell_exit
+};
+
+int lsh_num_builtins() {
+  return sizeof(builtin_str) / sizeof(char *);
+}
+
+// Builtin function implementations.
+
+// Change directory.
+int shell_cd(char **args) {
+  if (args[1] == NULL) {
+    fprintf(stderr, "lsh: expected argument to \"cd\"\n");
+  } else {
+    if (chdir(args[1]) != 0) {
+      perror("lsh");
+    }
+  }
+  return 1;
+}
+
+// Help builtin command.
+int shell_help(char **args) {
+  int i;
+  printf("Shell-in-c's LSH\n");
+  printf("Type program names and arguments, and hit enter.\n");
+  printf("The following are built in:\n");
+
+  for (i = 0; i < lsh_num_builtins(); i++) {
+    printf("  %s\n", builtin_str[i]);
+  }
+
+  printf("Use the man command for information on other programs.\n");
+  return 1;
+}
+
+// Exit builtin command.
+int shell_exit(char **args) {
+  return 0;
+}
--- execute.c
+++ execute.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <fcntl.h>
+#include "builtin.c"
 
 #define MY_SH_TOK_DELIM " \t\r\n\a"
 #define ERROR "\033[1;31mmy_sh\033[0m"
@@ -11,27 +12,7 @@
 
 int execute(char **args, int fdin, int fdout);
 
-// Function Declarations for builtin shell commands:
-int lsh_cd(char **args);
-int lsh_help(char **args);
-int lsh_exit(char **args);
 
-// List of builtin commands, followed by their corresponding functions.
-char *builtin_str[] = {
-  "cd",
-  "help",
-  "exit"
-};
-
-int (*builtin_func[]) (char **) = {
-  &shell_cd,
-  &shell_help,
-  &shell_exit
-};
-
-int lsh_num_builtins() {
-  return sizeof(builtin_str) / sizeof(char *);
-}
 
 // arr copy
 char **arr_cpy(char **arr, int i, int bool) {
@@ -52,40 +33,6 @@ char **arr_cpy(char **arr, int i, int bool) {
   }
   
   return a_c;
-}
-
-// Builtin function implementations.
-
-// Change directory.
-int shell_cd(char **args) {
-  if (args[1] == NULL) {
-    fprintf(stderr, "lsh: expected argument to \"cd\"\n");
-  } else {
-    if (chdir(args[1]) != 0) {
-      perror("lsh");
-    }
-  }
-  return 1;
-}
-
-// Help builtin command.
-int shell_help(char **args) {
-  int i;
-  printf("Shell-in-c's LSH\n");
-  printf("Type program names and arguments, and hit enter.\n");
-  printf("The following are built in:\n");
-
-  for (i = 0; i < lsh_num_builtins(); i++) {
-    printf("  %s\n", builtin_str[i]);
-  }
-
-  printf("Use the man command for information on other programs.\n");
-  return 1;
-}
-
-// Exit builtin command.
-int shell_exit(char **args) {
-  return 0;
 }
 
 // Check < value in args
