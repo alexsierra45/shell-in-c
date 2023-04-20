2fc4f2b0959e287290f3633ebd959c9384fab618
Alex <alexsierraalcala@gmail.com>
2023-04-20 11:39:02 -0400

    condition without else fixed

--- execute.c
+++ execute.c
@@ -148,13 +148,13 @@ int conditions(char **args, int fdin, int fdout) {
     args = arr_cpy(args, i+1, 0);
     i = 0;
     while (strcmp(args[i], "else") != 0) {
+      i++;
       if (args[i] == NULL) {
-        char **a_then = arr_cpy(args, i-2, 1);
-        if (execute(a_if, fdin, fdout)) execute(a_then, fdin, fdout);
+        char **a_then = arr_cpy(args, i-1, 1);
+        if (execute(a_if, fdin, fdout) == 0) execute(a_then, fdin, fdout);
 
         return 0;
       }
-      i++;
     }
     char **a_then = arr_cpy(args, i, 1);
     args = arr_cpy(args, i+1, 0);
