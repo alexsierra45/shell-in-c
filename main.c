ec813c3ff8c010419a44805b4531f212e6201cf7
Alex <alexsierraalcala@gmail.com>
2023-04-24 17:06:51 -0400

    empty the file background.txt before the loop

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
+#include <stdio.h>
 #include "loop_functions.c"
 
 int main(int arcg, char *argv[]) {
+
+    // Empty the file background.txt
+    FILE *f = fopen("background/background.txt", "w");
+    fclose(f);
 
     loop();
 
