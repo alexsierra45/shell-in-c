82829ee2987bf40b748ed006012b17e56ce43a8b
Alex <alexsierraalcala@gmail.com>
2023-04-25 01:54:44 -0400

    init function

--- main.c
+++ main.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include "loop_functions.c"
 
-int main(int arcg, char *argv[]) {
-
+void init() {
     // Empty the file background.txt
     FILE *f = fopen("background/jobs.txt", "w");
     fclose(f);
+}
 
+int main(int arcg, char *argv[]) {
+
+    init();
     loop();
 
     return 0;
